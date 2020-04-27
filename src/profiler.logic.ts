3debaa48e41e0f901b75cc51ee2b4403294c0d8e
Wojciech Cendrzak <>
2020-04-27 02:14:07 +0200

    code coverage up

--- src/profiler.logic.ts
+++ src/profiler.logic.ts
@@ -1,4 +1,6 @@
-﻿export const getFullName = (parentFullName: string | undefined, name: string): string => {
+﻿import { isDate } from 'util';
+
+export const getFullName = (parentFullName: string | undefined, name: string): string => {
   return `${parentFullName || ''} -> ${name}`;
 };
 
@@ -6,19 +8,4 @@ export const diff = (from: Date | null | undefined, to: Date | null | undefined)
   if (!from || !to || from === null || to === null || !isDate(from) || !isDate(to)) return 0;
 
   return to.getTime() - from.getTime();
-};
-
-const isDate = (value: Date | number | string) => {
-  switch (typeof value) {
-    case 'number':
-      return true;
-    case 'string':
-      return !isNaN(Date.parse(value));
-    case 'object':
-      if (value instanceof Date) {
-        return !isNaN(value.getTime());
-      }
-    default:
-      return false;
-  }
 };
